4f096222b146fef21b95718c4101aec679e31937
Tama Topik <148953229+devtama101@users.noreply.github.com>
2024-07-02 11:20:24 +0700

    adding cors

--- app.js
+++ app.js
@@ -3,17 +3,26 @@ const express = require('express');
 const connectDB = require('./config/db');
 const itemRoutes = require('./routes/itemRoutes');
 const errorHandler = require('./middleware/errorHandler');
+const cors = require('cors');
+const morgan = require('morgan');
 
 const app = express();
 
+
 // Connect to MongoDB
 connectDB();
+
+// Enable CORSconst 
+app.use(cors());
 
 // Middleware
 app.use(express.json());
 
 // Routes
 app.use('/items', itemRoutes);
+
+// Dev logging middleware
+app.use(morgan('dev'));
 
 // Error handling middleware
 app.use(errorHandler);
